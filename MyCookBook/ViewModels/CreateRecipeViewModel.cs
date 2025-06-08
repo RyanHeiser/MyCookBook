5d9e454a9be041f66124a649bf03e1d120f119d9
RyanHeiser <rmheiser808@gmail.com>
2025-06-08 14:47:34 -0700

    RecipeDisplayViewModel and CreateRecipeViewModel now extend a RecipeViewModelBase

--- MyCookBook/ViewModels/CreateRecipeViewModel.cs
+++ MyCookBook/ViewModels/CreateRecipeViewModel.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using MyCookBook.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyCookBook.ViewModels
 {
-    class CreateRecipeViewModel : ViewModelBase
+    class CreateRecipeViewModel : RecipeViewModelBase
     {
+        public CreateRecipeViewModel(Recipe recipe) : base(recipe)
+        {
+
+        }
     }
 }
