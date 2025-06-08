b980fe608ca850a165341afa65b4f9a1f6a4cc8c
RyanHeiser <rmheiser808@gmail.com>
2025-06-07 23:04:06 -0700

    Basic CreateRecipeView made.

---
+++ MyCookBook/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MyCookBook.Views;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyCookBook;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.Show();
+        base.OnStartup(e);
+    }
+}
+
