b1a2021b807a2fbd73f76925549a648fe4092b3e
RyanHeiser <rmheiser808@gmail.com>
2025-08-08 03:25:19 -0700

    Added validation to name text box in CreateRecipeView for when textbox is empty.

---
+++ MyCookBook.WPF/Validation/EmptyStringValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MyCookBook.WPF.Validation
+{
+    class EmptyStringValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrEmpty(value as string))
+                return new ValidationResult(false, "Must not be empty");
+            else
+                return new ValidationResult(true, null);
+        }
+    }
+}
